a1ca1d812cde60aa4700c01ac249feb5a59b51d9
JuanD <juand.herrera@hotmail.com>
2018-05-11 10:37:27 -0500

    Upload

--- src/test/java/com/ceibaParking/application/service/parkingServiceTest.java
+++ src/test/java/com/ceibaParking/application/service/parkingServiceTest.java
@@ -31,25 +31,11 @@ public class parkingServiceTest {
 		Car car = new Car("MMY000",formatter.parse(startDate),new Date());
 		// Act
 		boolean registerCar = parkingService.registerCar(car);
-		//Mockito.when(parkingService.registerCar(car)).thenReturn(true);
 		// Assert
 		assertTrue(registerCar);
 		assertEquals(car.getLicencePlate(), parkingService.findByPlate("MMY000").getLicencePlate());
 	}
 
-//	@Test
-//	public void registerMotorcycleService() {
-//		// Arrange
-//		
-//		Motorcycle motorcycle = Motorcycle.builder()
-//				.licencePlate("MMY000A")
-//				.startTime(new Date())
-//				.endTime(new Date())
-//				.cubicCentimeters(125).build();
-//		Mockito.when(parkingService.registerMotorcycle(motorcycle)).thenReturn(true);
-//		boolean registerCar = parkingService.registerMotorcycle(motorcycle);
-//		// Assert
-//		assertTrue(registerCar);
-//	}
+	
 
 }
