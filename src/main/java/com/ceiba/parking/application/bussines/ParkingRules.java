fa9505ad620c8299e1b94190a31bd39a85dbd31b
JuanD <juand.herrera@hotmail.com>
2018-05-22 15:06:21 -0500

    Actualizacion de test

--- src/main/java/com/ceiba/parking/application/bussines/ParkingRules.java
+++ src/main/java/com/ceiba/parking/application/bussines/ParkingRules.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import com.ceiba.parking.application.domain.Vehicle;
 
 public interface ParkingRules {
-	public int CAR_CAPACITY = 200;
+	public int CAR_CAPACITY = 20;
 	public int MOTORCYCLE_CAPACITY = 10;  
 	public boolean validateRegister(Vehicle vehicle, Date starTime);	
 	public void plateRestriction(Vehicle vehicle, Date startTime);
